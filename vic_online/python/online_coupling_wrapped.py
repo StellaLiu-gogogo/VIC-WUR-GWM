6c23fcd16cb51aa6996a81a83b52d165c34cdcdb
StellaLiu-gogogo <lsd920608@gmail.com>
2023-11-06 15:37:52 +0100

    update

--- vic_online/python/online_coupling_wrapped.py
+++ vic_online/python/online_coupling_wrapped.py
@@ -20,18 +20,19 @@ vic_executable = '/home/sliu/Documents/vic_indus/99SourceCode/VIC-WUR-GWM-1910/v
 
 #%%
 startstamp = datetime(1968, 1, 1)
-current_date = startstamp
-finishdate = datetime(1969, 1, 31)
+current_date = datetime(1968,1,1)
+finishdate = datetime(1975, 12, 31)
 
 # Loop over the dates
 while current_date <= finishdate:
     print("Running VIC for the time step [{}-{}]".format(current_date.year, current_date.month))
+    #generate dmy for VIC run
     (startyear , startmonth , startday,
     endyear , endmonth , endday, 
     stateyear , statemonth , stateday, 
     init_date , init_datestr) = sf.prepare_dates(current_date)   
          
-    
+    # prepare VIC config file
     config_file = vr.prepare_vic(startyear, startmonth, startday, 
                endyear, endmonth, endday, 
                stateyear, statemonth, stateday, 
@@ -39,17 +40,24 @@ while current_date <= finishdate:
                startstamp, filepath, statefile_dir, 
                configfile_dir, vic_executable)    
     vr.run_vic(vic_executable, config_file, startyear, startmonth)    
-    #vr.PostProcessVIC()
+   
     
-    #mf.PrepareMF()
+    #vr.PostProcessVIC()  prepare the OUT_GWRECHARGE, OUT_DISCHARGE, and OUT_NON_REN_SECT into modflow input unit conversion
     
-    #mf.RunMF()
+    #mf.PrepareMF()  prepare other modlofw inputs   finished. 
     
-    #mf.PostProcessMF()
+    #mf.RunMF() basically is already there
+    
+    #mf.PostProcessMF() 
+        #1. prepare GWD data to identfy conditon of GWD and VIC soil layer depth
+        
+        #2. export Baseflow data from Water Budget output, save it as a netcdf file for VIC to read in the next time step
     
     #mf.Feedback2VIC()
+        #1. read the current soil moisture and update it based on the condition. 
     
     #vr.update_statefile()
+        #1. update the statefile's soil moisture for the next time step. 
     
     #move to next time step
     current_date += relativedelta(months=1)
--- vic_online/python/support_function.py
+++ vic_online/python/support_function.py
@@ -7,10 +7,13 @@ def prepare_dates(current_date):
     _, lastday = calendar.monthrange(startyear, startmonth)  # to get the last day of the month
 
     statedate = datetime(startyear, startmonth, lastday) # to get the state as a datetime object
-    stateyear, statemonth, stateday = statedate.year, statedate.month, statedate.day #to specify config file
+    #stateyear, statemonth, stateday = statedate.year, statedate.month, statedate.day #to specify config file
+    stateyear, statemonth, stateday = (current_date + relativedelta(months=1)).year, (current_date + relativedelta(months=1)).month, 1
 
-    init_date = current_date - relativedelta(days = 1)
-    init_datestr = init_date.strftime('%Y%m%d')  #for generating statefile name  
+    init_date = current_date 
+    #init_date = current_date - relativedelta(days = 1)
+    #init_datestr = init_date.strftime('%Y%m%d')  #for generating statefile name  
+    init_datestr = current_date.strftime('%Y%m%d')  #for generating statefile name  
 
     endyear, endmonth, endday = (current_date + relativedelta(months=1)).year, (current_date + relativedelta(months=1)).month, 1
 
